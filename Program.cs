344431ec8e12808f4118a09f202c8343e3ac7b1d
«Roman <roman.anisimov2000@mail.ru>
2023-09-03 15:17:28 +0300

    Написал метод который принимает принимает массив строк и размер искомой строки в качестве своих параметров, а затем создает новый массив строк с той же длиной.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] FilterShortStrings(string [] arr, int length)
+{
+    string [] newArr = new string[arr.Length];
+    int index = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Trim().Length <= length)
+        {
+            newArr[index] = arr[i];
+            index++;
+        }   
+    }
+    return newArr;
+}
